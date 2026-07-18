4af3608d39fdb3d23c5f5fc26597cfc8bf037415
Kaio Andrião Dalfior <kaio.andriao.dalfior@gmail.com>
2026-07-18 05:49:35 -0300

    Metodo realizar login criado.

--- src/main/java/com/novoprojeto/sitezinho/Controller/UsuarioController.java
+++ src/main/java/com/novoprojeto/sitezinho/Controller/UsuarioController.java
@@ -7,10 +7,12 @@ import org.springframework.web.bind.annotation.PostMapping;
 import com.novoprojeto.sitezinho.Model.Usuario;
 import com.novoprojeto.sitezinho.Repositorio.UsuarioRepositorio;
 
+import java.util.Optional;
 import org.springframework.ui.Model;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 //diz ao Spring que este método é um controlador.
@@ -21,6 +23,8 @@ public class UsuarioController {
     private UsuarioRepositorio usuarioRepositorio;
 
 
+
+    //rota de registro
     @PostMapping("/registro")
     public String salvar(Usuario usuario) {
 
@@ -35,6 +39,33 @@ public class UsuarioController {
         model.addAttribute("usuario", new Usuario());
         return "registro";
     }
-    
 
+    //rota de login
+    @GetMapping("/login")
+    public String abrirLogin(){
+        return "login";
+    }
+    //realizar login
+    @PostMapping("/login")
+    public String realizarLogin(String email, String senha, Model model) {
+        Optional<Usuario> usuario = usuarioRepositorio.findByEmail(email);
+
+        if(usuario.isPresent()){
+            Usuario usuarioBanco = usuario.get();
+
+            if(usuarioBanco.getSenha().equals(senha)){
+                return "redirect:/home";
+            }
+        }
+
+        model.addAttribute("erro", "E-mail ou senha inválidos!");
+
+        return "login";
+    }
+    
+    //rota de Home
+    @GetMapping("/home")
+    public String abrirHome(){
+        return "home";
+    }
 }
--- src/main/java/com/novoprojeto/sitezinho/Repositorio/UsuarioRepositorio.java
+++ src/main/java/com/novoprojeto/sitezinho/Repositorio/UsuarioRepositorio.java
@@ -1,9 +1,12 @@
 package com.novoprojeto.sitezinho.Repositorio;
 
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.novoprojeto.sitezinho.Model.Usuario;
 
-public interface UsuarioRepositorio extends JpaRepository<Usuario, Long>{
-    
+public interface UsuarioRepositorio extends JpaRepository<Usuario, Long>
+{
+   
+    Optional<Usuario> findByEmail(String email);    
+    //Ao usar o findByEmail, o JPA do Spring data já entende que é para buscar por email. 
 }
